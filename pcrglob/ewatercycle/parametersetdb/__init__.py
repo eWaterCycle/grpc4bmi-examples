2128e6cfa07c65bfbdf67f5bdb54c44fc2b93215
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-07-19 17:16:25 +0200

    Use Python library to fetch and prepare input for model

---
+++ pcrglob/ewatercycle/parametersetdb/__init__.py
@@ -0,0 +1,37 @@
+import configparser
+import requests
+from subprocess import check_call
+
+
+class CaseConfigParser(configparser.ConfigParser):
+    def optionxform(self, optionstr):
+        return optionstr
+
+
+class Parameterset():
+    config_source = 'https://github.com/UU-Hydro/PCR-GLOBWB_input_example/raw/master/RhineMeuse30min/ini_and_batch_files/rapid/setup_natural_test.ini'
+    datafiles_source = 'https://github.com/UU-Hydro/PCR-GLOBWB_input_example/trunk/RhineMeuse30min'
+
+    def __init__(self):
+        self._init_config()
+
+    def save_datafiles(self, target):
+        check_call(['svn', 'export', self.datafiles_source, target])
+
+    def _init_config(self):
+        self.config = CaseConfigParser()
+        r = requests.get(self.config_source)
+        r.raise_for_status()
+        self.config.read_string(r.text)
+
+    def save_config(self, fn):
+        with open(fn, 'w') as f:
+            self.config.write(f)
+
+
+class Parametersetdb():
+    def select(self, model, name):
+        if model == 'PCR-GLOBWB' and name == 'RhineMeuse30min':
+            return Parameterset()
+        else:
+            raise FileNotFoundError()
